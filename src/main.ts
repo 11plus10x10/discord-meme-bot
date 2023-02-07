0112d04665f9e84eb195a09f5a5b09b45869935e
Alexandr Iurashchuk <a.iurashchuk@gmail.com>
2023-02-07 19:57:07 +0100

    + deterministic cooldowns and playback, but code needs a huge cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import { Client, Events, IntentsBitField, VoiceBasedChannel } from "discord.js";
 import {
+    AudioPlayer,
     AudioPlayerStatus,
     createAudioPlayer,
     createAudioResource,
@@ -8,12 +9,22 @@ import {
     joinVoiceChannel,
     VoiceConnectionStatus
 } from "@discordjs/voice";
+import * as dayjs from "dayjs";
 
 dotenv.config({ path: "../.env" });
 
-let connectedToday = false;
+const cooldownStates = {
+    [process.env.LEGEND]: {
+        onCooldown: false,
+        timer: dayjs(),
+    },
+    [process.env.PRINCESS]: {
+        onCooldown: false,
+        timer: dayjs(),
+    }
+};
 
-async function connectToChannel(channel: VoiceBasedChannel) {
+async function connectToChannel(channel: VoiceBasedChannel, whoHasConnected: string) {
     const connection = joinVoiceChannel({
         channelId: channel.id,
         guildId: channel.guild.id,
@@ -21,7 +32,8 @@ async function connectToChannel(channel: VoiceBasedChannel) {
     });
     try {
         await entersState(connection, VoiceConnectionStatus.Ready, 30_000);
-        connectedToday = true;
+        cooldownStates[whoHasConnected].timer = dayjs().add(2, "minute");
+        cooldownStates[whoHasConnected].onCooldown = true;
         return connection;
     } catch (e) {
         connection.destroy();
@@ -29,41 +41,38 @@ async function connectToChannel(channel: VoiceBasedChannel) {
     }
 }
 
-//  ==========================================PLAYER==========================================
-const player = createAudioPlayer();
+function cooldownCheck(whoToCheck: string){
+    if (dayjs().isAfter(cooldownStates[whoToCheck].timer)) cooldownStates[whoToCheck].onCooldown = false;
+}
 
-function playSong() {
-    // const resource = createAudioResource("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3");
-    const resource = createAudioResource("../assets/legend.ogg");
+function playSong(player: AudioPlayer, path: string) {
+    const resource = createAudioResource(path);
     player.play(resource);
     return entersState(player, AudioPlayerStatus.Playing, 5000);
 }
 
-//  ==========================================PLAYER==========================================
-
-
 const client = new Client({ intents: [IntentsBitField.Flags.GuildVoiceStates, IntentsBitField.Flags.Guilds] });
 
-client.on("ready", async () => {
-    try {
-        await playSong();
-    } catch (e) {
-        console.error(e);
-    }
-});
-
-
 client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
-    if (connectedToday) return;
-    if (oldState.channelId === null && oldState.member.user.tag == process.env.LEGEND) {
-        console.log(`Dmytro is happy to see ${oldState.member.user.tag}`);
+    //  Terminate if event is not triggered by one of preferred users;
+    const { LEGEND, PRINCESS } = process.env;
+    const { tag } = oldState.member.user;
+    if (!([LEGEND, PRINCESS]).includes(tag)) return;
+    cooldownCheck(tag);
+
+    if (cooldownStates[tag].onCooldown) return;
+    const path = tag === LEGEND ? "../assets/legend.ogg" : "../assets/princess.ogg";
+    if (oldState.channelId === null) {
         const channel = newState.member.voice.channel;
         if (channel) {
             try {
-                const connection = await connectToChannel(channel);
+                const player = createAudioPlayer();
+                const connection = await connectToChannel(channel, tag);
                 connection.subscribe(player);
+                await playSong(player, path);
                 await entersState(player, AudioPlayerStatus.Idle, 60000);
                 connection.destroy();
+                player.stop();
             } catch (e) {
                 console.error(e);
             }
